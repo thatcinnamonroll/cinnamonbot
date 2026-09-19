10858d24b628fba30bf5aaaaf42044db8ef5dafa
Mateusz <thatcinnamonroll@tutamail.com>
2026-09-19 20:28:36 +0200

    very basic music playing works

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import json
 import discord
 from discord.ext import commands
 import os
-from utils.youtube import downloadSong
+from utils.youtube import youtube
+import time
 
 botDir = os.getcwd()
+# 0 is a placeholder value
+voice = {"vc":0, "isPlaying":False,"queue":[]}
 
 with open(".data/settings.json","r") as settingsFile:
     settings = json.load(settingsFile)
@@ -19,18 +22,55 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 @bot.command()
 async def startsong(ctx,songUrl):
     if ctx.author.voice:
-        await ctx.send("Holdup, setting up")
-        channel = ctx.author.voice.channel
-        downloadSong(songUrl,botDir)
-        client = await channel.connect()
-        # source = await discord.FFmpegOpusAudio.from_probe("")
-        client.play(source)
-        await ctx.send("Done")
+        if not voice["isPlaying"]:
+            await ctx.send("Holdup, setting up")
+            channel = ctx.author.voice.channel
+            youtube.downloadSong(songUrl,botDir)
+            songData = youtube.getMusicData(songUrl)
+
+            client = await channel.connect()
+            voice["vc"] = client
+            voice["isPlaying"] = True
+
+            source = await discord.FFmpegOpusAudio.from_probe(f".cache/music/{songData["id"]}.opus")
+            client.play(source)
+            await ctx.send("Done")
+        else:
+            songData = youtube.getMusicData(songUrl)
+            voice["queue"].append(songData["id"])
+            await ctx.send(f"Something is already plaing, adding {songData["name"]} to queue")
     else:
         await ctx.send('Cant play, you are not on any voice channel')
 
+# wow how do you work
+@bot.command()
+async def pause(ctx):
+    if voice["isPlaying"]:
+        vc = voice["vc"]
+        vc.pause()
+        await ctx.send("Music is paused")
+    else:
+        await ctx.send("Nothing is playing")
 
+@bot.command()
+async def resume(ctx):
+    if voice["isPlaying"]:
+        vc = voice["vc"]
+        vc.resume()
+        await ctx.send("Resuming")
+    else:
+        await ctx.send("Nothing is playing")
 
+@bot.command()
+async def stop(ctx):
+    if voice["isPlaying"]:
+        vc = voice["vc"]
+        vc.stop()
+        await ctx.send("Stoping")
+        await vc.disconnect()
+        voice = {"vc":0, "isPlaying":False}
+    else:
+        await ctx.send("Nothing is playing")
 
 @bot.event
 async def on_message(message):
--- utils/youtube.py
+++ utils/youtube.py
@@ -1,21 +1,39 @@
 import yt_dlp as ytdlp
 
-def downloadSong(url,botDir):
-    ytdlpOptions = {'extract_flat': 'discard_in_playlist',
-    'format': 'bestaudio/best',
-    'fragment_retries': 10,
-    'outtmpl': {'default': '%(id)s.%(ext)s'},
-    'ignoreerrors': 'only_download',
-    'paths': {'home': f"{botDir}/.cache/music"},
-    'postprocessors': [{'key': 'FFmpegExtractAudio',
-                         'nopostoverwrites': False,
-                         'preferredcodec': 'opus',
-                         'preferredquality': '5'},
-                        {'key': 'FFmpegConcat',
-                         'only_multi_video': True,
-                        'when': 'playlist'}],
-    'retries': 10}
+class youtube:
+    def downloadSong(url,botDir):
+        ytdlpOptions = {'extract_flat': 'discard_in_playlist',
+        'format': 'bestaudio/best',
+        'fragment_retries': 10,
+        'outtmpl': {'default': '%(id)s.%(ext)s'},
+        'ignoreerrors': 'only_download',
+        'paths': {'home': f"{botDir}/.cache/music"},
+        'postprocessors': [{'key': 'FFmpegExtractAudio',
+                            'nopostoverwrites': False,
+                            'preferredcodec': 'opus',
+                            'preferredquality': '5'},
+                            {'key': 'FFmpegConcat',
+                            'only_multi_video': True,
+                            'when': 'playlist'}],
+        'retries': 10}
 
-    urlList = [url]
-    ytdlp.YoutubeDL(ytdlpOptions).download(urlList)
+        urlList = [url]
+        ytdlp.YoutubeDL(ytdlpOptions).download(urlList)
+
+    def getMusicData(url):
+        musicData = {}
+
+        ydlpOpts = {
+            'quiet': True,
+            'skip_download': True,
+        }
+
+        with ytdlp.YoutubeDL(ydlpOpts) as ydl:
+            info = ydl.extract_info(url, download=False)
+
+        musicData["id"] = info["id"]
+        musicData["title"] = info["title"]
+        musicData["author"] = info["uploader"]
+
+        return musicData
 
